cb00515c3333407d03252f7d5c8ab3a16f4b2aa0
Максим Барабанов <maxim.barabanov@gmail.com>
2019-01-11 09:46:29 +0300

    dx components installed

--- js/src/components/SocialAuth.vue
+++ js/src/components/SocialAuth.vue
@@ -1,12 +1,22 @@
 <template>
     <div class="social_auth">
-      <div class="social_auth__provider social_auth__vk"><a href="/auth/login/vk-oauth2">Вконтакте</a> </div>
+      <div class="social_auth__provider social_auth__vk"><a href="/auth/login/vk-oauth2">VK</a> </div>
+      <dx-button :text="text"/>
     </div>
 </template>
 
 <script>
+import DxButton from 'devextreme-vue/button'
 export default {
-  name: 'SocialAuth'
+  name: 'SocialAuth',
+  data () {
+    return {
+      text: 'test'
+    }
+  },
+  components: {
+    DxButton
+  }
 }
 </script>
 
--- js/src/main.js
+++ js/src/main.js
@@ -3,6 +3,8 @@
 import Vue from 'vue'
 import App from './App'
 import router from './router'
+import 'devextreme/dist/css/dx.common.css'
+import 'devextreme/dist/css/dx.light.compact.css'
 
 Vue.config.productionTip = false
 
